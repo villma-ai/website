8f7b4abe8f094f8c4fb0358420a2ab7dd910c71e
Christian Zanchetta <>
2025-07-02 13:46:58 +0200

    debug NEXT_PUBLIC_SHOW_TEASER

--- src/app/(blank)/teaser/page.tsx
+++ src/app/(blank)/teaser/page.tsx
@@ -1,5 +1,7 @@
 import Teaser from '@/components/Teaser';
 
 export default function TeaserPage() {
+  console.log('NEXT_PUBLIC_SHOW_TEASER - server', process.env.NEXT_PUBLIC_SHOW_TEASER )
+
   return <Teaser />;
 }
